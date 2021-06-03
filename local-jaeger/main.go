41c6d09454de49f06f3b0c009255093aa3984659
wookiist <kyle.oh95@gmail.com>
2021-06-03 16:24:21 +0900

    FEAT: Add '/child' api

--- local-jaeger/main.go
+++ local-jaeger/main.go
@@ -1,21 +1,39 @@
 package main
+
 import (
-    "github.com/labstack/echo-contrib/jaegertracing"
-    "github.com/labstack/echo/v4"
-    "net/http"
-    "time"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo-contrib/jaegertracing"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
+
 func main() {
-    e := echo.New()
-    // Enable tracing middleware
-    c := jaegertracing.New(e, nil)
-    defer c.Close()
-    e.GET("/", func(c echo.Context) error {
-        // Wrap slowFunc on a new span to trace it's execution passing the function arguments
+	e := echo.New()
+	// Enable tracing middleware
+	c := jaegertracing.New(e, nil)
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	defer c.Close()
+	e.GET("/", func(c echo.Context) error {
+		// Wrap slowFunc on a new span to trace it's execution passing the function arguments
 		jaegertracing.TraceFunction(c, slowFunc, "Test String")
-        return c.String(http.StatusOK, "Hello, World!")
-    })
-    e.Logger.Fatal(e.Start(":1323"))
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.GET("/child", func(c echo.Context) error {
+		// Do something before creating the child span
+		time.Sleep(40 * time.Millisecond)
+		sp := jaegertracing.CreateChildSpan(c, "Child span for additional processing")
+		defer sp.Finish()
+		sp.LogEvent("Test log")
+		sp.SetBaggageItem("Test baggage", "baggage")
+		sp.SetTag("test tag", "Istio Study")
+		time.Sleep(100 * time.Millisecond)
+		return c.String(http.StatusOK, "Hello, Child!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
 
 // A function to be wrapped. No need to change it's arguments due to tracing
